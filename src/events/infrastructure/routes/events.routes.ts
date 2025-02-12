76e7f2cfb7384dc74d84cf4cc1844cab86c8dff9
CamiloCR96 <Camilocarrion9626@gmail.com>
2025-02-11 23:34:34 -0500

    fix: Update routes in swagger Event

--- src/events/infrastructure/routes/events.routes.ts
+++ src/events/infrastructure/routes/events.routes.ts
@@ -63,14 +63,9 @@ router.use(authenticateUser); // Apply the auth middleware to all routes
  *                         example: "Mercado de Juan"
  *                         description: Event name
  *                       productIds:
- *                       type: array
- *                       items:
- *                         type: string
- *                         properties:
- *                           id:
- *                             type: string
- *                             example: 202020202
- *                             description: Product ID
+ *                         type: array
+ *                         items:
+ *                           type: string
  *                 message:
  *                   type: string
  *                   example: "Events retrieved successfully"
@@ -169,11 +164,6 @@ router.get("/", getEvents);
  *                       type: array
  *                       items:
  *                         type: string
- *                         properties:
- *                           id:
- *                             type: string
- *                             example: 202020202
- *                             description: Product ID
  *                 message:
  *                   type: string
  *                   example: "Event retrieved successfully"
@@ -236,123 +226,36 @@ router.get("/:id", getEvent);
  *               id:
  *                 type: string
  *                 example: "665942a9-97b8-421e-a9b6-78d9ef8a87f1"
- *                 description: Event ID
  *               date:
  *                 type: string
  *                 example: "2025-01-30"
- *                 description: Event date
  *               completed:
  *                 type: boolean
  *                 example: false
- *                 description: Event completion status
  *               user_id:
  *                 type: string
  *                 example: "2fe143ac-e9d0-4878-80ef-27577ab0b91a"
- *                 description: User ID
  *               precio_total:
  *                 type: number
  *                 example: 0
- *                 description: Total price
  *               name:
  *                 type: string
  *                 example: "Mercado de Juan"
- *                 description: Event name
  *               productIds:
  *                 type: array
- *                  items:
+ *                 items:
  *                   type: string
- *                   properties:
- *                    id:
- *                     type: string
- *                      example: 202020202
- *                      description: Product ID
  *     responses:
  *       200:
  *         description: Event created successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: string
- *                       example: "665942a9-97b8-421e-a9b6-78d9ef8a87f1"
- *                       description: Event ID
- *                     date:
- *                       type: string
- *                       example: "2025-01-30"
- *                       description: Event date
- *                     completed:
- *                       type: boolean
- *                       example: false
- *                       description: Event completion status
- *                     user_id:
- *                       type: string
- *                       example: "2fe143ac-e9d0-4878-80ef-27577ab0b91a"
- *                       description: User ID
- *                     precio_total:
- *                       type: number
- *                       example: 0
- *                       description: Total price
- *                     name:
- *                       type: string
- *                       example: "Mercado de Juan"
- *                       description: Event name
- *                     productIds:
- *                       type: array
- *                       items:
- *                         type: string
- *                         properties:
- *                           id:
- *                             type: string
- *                             example: 202020202
- *                             description: Product ID
- *                 message:
- *                   type: string
- *                   example: "Event created successfully"
  *       400:
  *         description: Bad request
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                   example: "Please check the request!"
  *       401:
  *         description: Unauthorized
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                   example: "Unauthorized, token required!"
  *       404:
  *         description: Not found
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                   example: "Resource not found!"
  *       500:
  *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                   example: "Internal unknown error has occurred!"
  */
 router.post("/", addEventWithProducts);
 
