24824c649ce4c49968e2e38036a20becc80ed33d
CamiloCR96 <Camilocarrion9626@gmail.com>
2025-02-11 23:01:34 -0500

    feat: Implement typing for user, event and middleware

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
 import { Request } from "express";
 import { Category } from "./categories/domain/entities/category";
 import { Product } from "./products/domain/entities/product";
+import { DomainEvent } from "./events/domain/entities/domainEvent";
+import { User } from "./auth/domain/entities/user";
 
 export enum STATUS_CODES {
   s200 = 200,
@@ -15,6 +17,10 @@ export enum STATUS_CODES {
 export interface AuthenticatedRequest extends Request {
   user?: {
     id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    birthday: string;
   };
 }
 
@@ -35,3 +41,15 @@ export type ProductListResponse = Promise<Product[]>;
 export type ProductPartialResponse = Partial<Product>;
 export type ProductResponseOrNull = Promise<Product | null>;
 /** ******* END OF: PRODUCT CONTEXT ******* */
+
+/** ******* EVENT CONTEXT ******* */
+export type EventResponse = Promise<DomainEvent>;
+export type EventListResponse = Promise<DomainEvent[]>;
+export type EventPartialResponse = Partial<DomainEvent>;
+export type EventResponseOrNull = Promise<DomainEvent | null>;
+/** ******* END OF: EVENT CONTEXT ******* */
+
+/** ******* USER CONTEXT ******* */
+export type UserResponse = Promise<User>;
+export type UserResponseOrNull = Promise<User | null>;
+/** ******* END OF: USER CONTEXT ******* */
