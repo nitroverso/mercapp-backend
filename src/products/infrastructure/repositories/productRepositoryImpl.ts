4fe7e4cb60274e62b50fe886721741ce754fa446
CamiloCR96 <Camilocarrion9626@gmail.com>
2025-02-26 10:19:08 -0500

    feat: Implementation buildRepository

--- src/products/infrastructure/repositories/productRepositoryImpl.ts
+++ src/products/infrastructure/repositories/productRepositoryImpl.ts
@@ -8,40 +8,38 @@ import {
   ProductResponse,
   ProductResponseOrNull,
 } from "../../../types";
+import { buildRepository } from "../../../utils/utils";
+
+const TABLE_NAME = TABLES.PRODUCTS;
 
 export class ProductRepositoryImpl implements IProductRepository {
   async findAll(userId: string): ProductListResponse {
-    const { data, error } = await supabase
-      .from(TABLES.PRODUCTS)
-      .select("*")
-      .eq(COLUMNS.USER_ID, userId);
-
-    if (error) throw new Error(error.message);
-    return data as Product[];
+    const supabaseCall = async () => {
+      return await supabase
+        .from(TABLE_NAME)
+        .select("*")
+        .eq(COLUMNS.USER_ID, userId);
+    };
+    return buildRepository<Product[]>({ supabaseCall });
   }
 
   async findById(id: string, userId: string): ProductResponseOrNull {
-    const { data, error } = await supabase
-      .from(TABLES.PRODUCTS)
-      .select("*")
-      .eq(COLUMNS.ID, id)
-      .eq(COLUMNS.USER_ID, userId)
-      .single();
-
-    if (error) return null;
-    return data as Product;
+    const supabaseCall = async () => {
+      return await supabase
+        .from(TABLE_NAME)
+        .select("*")
+        .eq(COLUMNS.ID, id)
+        .eq(COLUMNS.USER_ID, userId)
+        .single();
+    };
+    return buildRepository<Product | null>({ supabaseCall });
   }
 
   async save(product: Product): ProductResponse {
-    const { data, error } = await supabase
-      .from(TABLES.PRODUCTS)
-      .insert(product)
-      .select()
-      .single();
-
-    if (error) throw new Error(error.message);
-
-    return data as Product;
+    const supabaseCall = async () => {
+      return await supabase.from(TABLE_NAME).insert(product).select().single();
+    };
+    return buildRepository<Product>({ supabaseCall });
   }
 
   async update(
@@ -49,29 +47,28 @@ export class ProductRepositoryImpl implements IProductRepository {
     userId: string,
     product: ProductPartialResponse
   ): ProductResponse {
-    const { data, error } = await supabase
-      .from(TABLES.PRODUCTS)
-      .update(product)
-      .eq(COLUMNS.ID, id)
-      .eq(COLUMNS.USER_ID, userId)
-      .select("*")
-      .single();
-
-    if (error) throw new Error(error.message);
-
-    return data as Product;
+    const supabaseCall = async () => {
+      return await supabase
+        .from(TABLE_NAME)
+        .update(product)
+        .eq(COLUMNS.ID, id)
+        .eq(COLUMNS.USER_ID, userId)
+        .select("*")
+        .single();
+    };
+    return buildRepository<Product>({ supabaseCall });
   }
 
   async delete(id: string, userId: string): ProductResponse {
-    const { data, error } = await supabase
-      .from(TABLES.PRODUCTS)
-      .delete()
-      .eq(COLUMNS.ID, id)
-      .eq(COLUMNS.USER_ID, userId)
-      .select("*")
-      .single();
-
-    if (error) throw new Error(error.message);
-    return data as Product;
+    const supabaseCall = async () => {
+      return await supabase
+        .from(TABLE_NAME)
+        .delete()
+        .eq(COLUMNS.ID, id)
+        .eq(COLUMNS.USER_ID, userId)
+        .select("*")
+        .single();
+    };
+    return buildRepository<Product>({ supabaseCall });
   }
 }
