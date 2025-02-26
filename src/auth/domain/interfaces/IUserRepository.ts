483c1c1ee249f87733d32877416f3db4131b7355
CamiloCR96 <Camilocarrion9626@gmail.com>
2025-02-26 10:07:29 -0500

    fix: service with repository was implemented in addition to adding build Repository and handlerError.

--- src/auth/domain/interfaces/IUserRepository.ts
+++ src/auth/domain/interfaces/IUserRepository.ts
@@ -1,7 +1,14 @@
-import { PromiseVoid, UserResponseOrNull } from "../../../types";
+import { AuthError, AuthUser } from "@supabase/supabase-js";
+import { UserResponse } from "../../../types";
 import { User } from "../entities/user";
 
 export interface IUserRepository {
-  findById(id: string): UserResponseOrNull;
-  save(user: User): PromiseVoid;
+  singUp(email: string, password: string): Promise<{ user: AuthUser | null }>;
+  loginSupabase(
+    email: string,
+    password: string
+  ): Promise<{ user: AuthUser | null }>;
+  logout(): Promise<{ error: AuthError | null }>;
+  save(user: User): UserResponse;
+  findById(userId: string): UserResponse;
 }
