aef8fedb29ee04ba6f628060d44226685f003261
CamiloCR96 <Camilocarrion9626@gmail.com>
2025-03-04 21:55:07 -0500

    fix: Remove console log

--- src/units/infrastructure/controllers/unitController.ts
+++ src/units/infrastructure/controllers/unitController.ts
@@ -6,7 +6,6 @@ import { AuthenticatedRequest, STATUS_CODES } from "../../../types";
 export const getUnits = async (req: AuthenticatedRequest, res: Response) => {
   const callback = async () => {
     const data = await listUnits();
-    console.log({ data });
     if (data) {
       res.json(parseResponse({ code: STATUS_CODES.s200, data }));
     }
