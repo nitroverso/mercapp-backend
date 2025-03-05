6f197b7ce774634e73daac8cb63091ce2decd336
CamiloCR96 <Camilocarrion9626@gmail.com>
2025-03-04 21:54:54 -0500

    feat: Implementation buildRepository to UnitRepository

--- src/units/infrastructure/repositories/unitRepositoryImpl.ts
+++ src/units/infrastructure/repositories/unitRepositoryImpl.ts
@@ -3,11 +3,14 @@ import { Unit } from "../../domain/entities/unit";
 import supabase from "../../../config/supabase";
 import { TABLES } from "../../../constants/mpConstanst";
 import { UnitListResponse } from "../../../types";
+import { buildRepository } from "../../../utils/utils";
 
+const TABLE_NAME = TABLES.UNITS;
 export class unitRepositoryImpl implements IUnitRepository {
   async findAll(): UnitListResponse {
-    const { data, error } = await supabase.from(TABLES.UNITS).select("*");
-    if (error) throw new Error(error.message);
-    return data as Unit[];
+    const supabaseCall = async () => {
+      return await supabase.from(TABLE_NAME).select("*");
+    };
+    return buildRepository<Unit[]>({ supabaseCall });
   }
 }
